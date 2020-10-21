603291bdca43546c739bc2e9e437ad52e8f6b612
Sergio Montano <sergiomontano@BCs-MacBook-Pro-2.local>
2020-10-21 12:11:14 -0500

    star wars

---
+++ dataStructuresExercise.js
@@ -0,0 +1,55 @@
+//jedi
+// 1a
+const jedi = [];
+//1b
+console.log("Luke"[1]);
+//1c
+jedi.push("Obi-Wan Kenobi");
+//1d
+jedi.unshift("Yoda");
+//1e
+jedi[jedi.length] = "luke"
+console.log(jedi)
+//1f
+jedi.splice(1,1);
+console.log(jedi)
+//1g
+jedi.shift();
+console.log(jedi);
+//sithlords
+//2a
+const sithLords = ["Darth Vader","Darth Sidious", "Darth Maul"];
+//2b
+const imperialOfficers = ["Grand Moff Tarkin", "Orson Krennic"];
+//2c
+const starWarsVillains = sithLords.concat(imperialOfficers);
+
+//2b
+console.log (starWarsVillains.slice(0, 2));
+const
+
+//3a
+const droid = {
+  astromech: "R2-D2",
+  protocol: "C-3PO",
+  protocol:  "C-3PO",
+  assassin:  "IG-11"
+}
+
+    
+
+//3b
+console.log (droid["astromech"]);
+
+//3c
+console.log(droid.protocol);
+
+//3d
+droid.assassin = "IG-11";
+
+
+
+
+
+
+
